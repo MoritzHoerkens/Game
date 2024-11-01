961c5b3ecc734f150cc6e8db6d2e060203687322
Moritz H. <mhoerkens@gmail.com>
2024-11-01 20:16:08 +0100

    test

---
+++ backend/map/Grid.java
@@ -0,0 +1,12 @@
+package backend.map;
+
+public class Grid {
+    private final int LENGTH, WIDTH;// define the length and width of the grid
+
+    private Terrain[][]
+    public Grid (int l, int w) {
+        LENGTH = l;
+        WIDTH = w;
+        
+    }
+}
---
+++ backend/units/Units.java
@@ -0,0 +1,4 @@
+package backend.units;
+class Units{
+
+}
