c2ecf6cc9d09c9781784aa598d5dd437df42fa3a
Moritz H. <mhoerkens@gmail.com>
2024-11-01 21:12:49 +0100

    contsructors for grid and basic terrain types

--- backend/map/Grid.java
+++ backend/map/Grid.java
@@ -1,12 +1,29 @@
 package backend.map;
 
 public class Grid {
-    private final int LENGTH, WIDTH;// define the length and width of the grid
+    private Terrain squares[][];
 
-    private Terrain[][]
-    public Grid (int l, int w) {
-        LENGTH = l;
-        WIDTH = w;
-        
+    /**
+     * initializes a field with the given dimensions exclusivly using Terrain 0
+     */
+    public Grid(int length, int width) {
+        squares = new Terrain[length][width];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; i < width; j++) {
+                squares[i][j] = new Terrain(0);
+            }
+        }
+    }
+
+    /**
+     * initializes a field with the dimension of the given 2DArray using the
+     * contianing Terrains
+     */
+    public Grid(int[][] fields) {
+        for (int i = 0; i < fields.length; i++) {
+            for (int j = 0; i < fields[i].length; j++) {
+                squares[i][j] = new Terrain(fields[i][j]);
+            }
+        }
     }
 }
---
+++ backend/map/Terrain.java
@@ -0,0 +1,82 @@
+package backend.map;
+
+public class Terrain {
+    public final double MOVEMENT_COST; // the amount of movemnt needed to trverse the square; -1 for impassable
+    public final double DEFENSE_MODIFIER_MELEE; // the modifier on the amount of melee damage the unit occupying the
+                                                // square takes
+    public final double DEFENSE_MODIFIER_RANGED;// the modifier on the amount of ranged damage the unit occupying the
+                                                // square tales
+    public final double ATTACK_MODIFIER_MELEE; // the modifier on the amount of melee damage the unit occupying the
+                                               // square inflicts
+    public final double ATTACK_MODIFIER_RANGED;// the modifier on the amount of ranged damage the unit occupying the
+                                               // square inflicts
+    public final double FLANKING_MODIFIER; // appleis a multipier to the damage taken and inflicted by the unit while
+                                           // flanking
+    public final boolean SHOOT_OVER_ALLOWED;
+    //TODO directed modifiers, maybe only sefull for river since the protect only from one direction
+    // TODO probably better to outsorce to JSON
+    public Terrain(int type) {
+        switch (type) {
+            case 0:// level
+                /*
+                 * nothing unusual
+                 */
+                SHOOT_OVER_ALLOWED = true;
+                MOVEMENT_COST = 1;
+                DEFENSE_MODIFIER_MELEE = 1;
+                DEFENSE_MODIFIER_RANGED = 1;
+                ATTACK_MODIFIER_MELEE = 1;
+                ATTACK_MODIFIER_RANGED = 1;
+                FLANKING_MODIFIER = 1;
+                break;
+            case 1:// forest
+                /*
+                 * slightly slowed down melee combat and reduced ranged combat on defense and
+                 * offense
+                 */
+                SHOOT_OVER_ALLOWED = true;
+                MOVEMENT_COST = 1.5;
+                DEFENSE_MODIFIER_MELEE = 0.9;
+                DEFENSE_MODIFIER_RANGED = 0.8;
+                ATTACK_MODIFIER_MELEE = 0.9;
+                ATTACK_MODIFIER_RANGED = 0.8;
+                FLANKING_MODIFIER = 1;
+                break;
+            case 2:// mountain
+                /*
+                 * slowed down melee and ranged combat and flanking effectiveness is reduced,
+                 * high movement cosrt
+                 */
+                SHOOT_OVER_ALLOWED = false;
+                MOVEMENT_COST = 2.5;
+                DEFENSE_MODIFIER_MELEE = 0.6;
+                DEFENSE_MODIFIER_RANGED = 0.6;
+                ATTACK_MODIFIER_MELEE = 0.6;
+                ATTACK_MODIFIER_RANGED = 0.6;
+                FLANKING_MODIFIER = 0.5;
+                break;
+            case 3:// river
+                /*
+                 * significatly benefits the defender in melee combat and has no effect on ranged combat, flanking increaded to counter the dadvantages
+                 */
+                SHOOT_OVER_ALLOWED = true;
+                MOVEMENT_COST = 2;
+                DEFENSE_MODIFIER_MELEE = 0.4;
+                DEFENSE_MODIFIER_RANGED = 1;
+                ATTACK_MODIFIER_MELEE = 1.6;
+                ATTACK_MODIFIER_RANGED = 1;
+                FLANKING_MODIFIER = 1/DEFENSE_MODIFIER_MELEE;
+                break;
+            default:// should not happen
+                SHOOT_OVER_ALLOWED = true;
+                MOVEMENT_COST = 1;
+                DEFENSE_MODIFIER_MELEE = 1;
+                DEFENSE_MODIFIER_RANGED = 1;
+                ATTACK_MODIFIER_MELEE = 1;
+                ATTACK_MODIFIER_RANGED = 1;
+                FLANKING_MODIFIER = 1;
+                break;
+        }
+    }
+}
+
