49cd70ec1a5669d4defe7021341f6d19276bbf7c
Moritz Hoerkens <mhoerkens@gmail.com>
2024-11-02 09:01:18 +0100

    renamed unitsto unit

--- backend/units/Unit.java
+++ backend/units/Unit.java
@@ -2,14 +2,13 @@ package backend.units;
 
 import java.util.ArrayList;
 
-abstract class Units {
-    public double attackDamage;// damge done to the primary target
-    public double flankingAttackDamage;// damge done to flanking targets
-    public int range;
-    public int health;
-    public int movementPoints;
+abstract class Unit {
+    private double attackDamage;// damge done to the primary target
+    private double flankingAttackDamage;// damge done to flanking targets
+    private int range;
+    private double health;
+    private int movementPoints;
     public ArrayList<tags> tags = new ArrayList<tags>();
-
     /**
      * the direction should only be 0-up,1-right,2-below,3-left
      * 
@@ -20,15 +19,21 @@ abstract class Units {
     /**
      * deals the given damage to another unit
      */
-    public void dealDamage(int direction, int damage) {
-
+    public void dealDamage(Unit unit, double damage) {
+        unit.takeDamage(damage);
     }
-
+    public void takeDamage(double damage){
+        this.health-=damage;
+        this.die();
+    }
+    public void die(){
+        tags.append(tags.DEAD)
+    }
     /*
      * calculates the damage dealt to all units around it and calls dealDamage
      * is to be implemented by the specific unit
      */
-    public void attack() {
+    public void attack(Unit[][] units) {
 
     }
 
@@ -39,5 +44,5 @@ abstract class Units {
 enum tags {
     LARGE,
     SMALL,
-    FLEEING,
+    DEAD
 }
