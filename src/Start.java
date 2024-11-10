f50392a521e1a8a2a912130459e30e07dfed7645
Moritz Hoerkens <mhoerkens@gmail.com>
2024-11-10 16:50:18 +0100

    started implementing a GameWindow

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+import backend.units.Swordman;
+import frontend.GameWindow;
+
+public class Start {
+    public static void main(String[] args) throws Exception {
+        GameWindow x = new GameWindow();
+    }
+}
