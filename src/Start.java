5543b8f94d0d8ee1fe2e33a776308108c5de84d7
Moritz Hoerkens <mhoerkens@gmail.com>
2024-11-15 12:00:34 +0100

    layout

--- src/Start.java
+++ src/Start.java
@@ -1,10 +1,7 @@
-import java.util.ArrayList;
-
-import backend.units.Swordman;
 import frontend.GameWindow;
 
 public class Start {
     public static void main(String[] args) throws Exception {
-        GameWindow x = new GameWindow();
+        new GameWindow();
     }
 }
--- src/frontend/GameWindow.java
+++ src/frontend/GameWindow.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import backend.units.*;
 
 public class GameWindow extends JFrame {
     private JSplitPane unitFieldSplit;// holds the COntainer for the Units and the Playingfield
-    private JScrollPane unitPane;
+    private JPanel unitPane;
 
     public static void main(String[] args) {
         GameWindow x = new GameWindow();
@@ -41,19 +42,45 @@ public class GameWindow extends JFrame {
     }
 
     private void initailizeUnitCards() {
-        unitPane = new JScrollPane();
+        unitPane = new JPanel();
+        unitFieldSplit.setLeftComponent(unitPane);
         unitPane.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 5, 10, 5);
-        // initalize all buttons for unit recriutment
+        gbc.gridwidth = 9;
+        /*
+         * 0BBB0BBB0
+         * BBB ist ein button mit 3 Kästen
+         * und 0 ist ein paltzhalter für Padding
+         */
 
+        // initalize all buttons for unit recriutment
+        ArrayList<JButton> allUnitButtons = new ArrayList<JButton>();
+        allUnitButtons.add(getUnitRecruitmentButton("Test1", null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test2", null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test3", null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test4", null));
+        for (JButton i : allUnitButtons) {
+            GridBagConstraints tmp = new GridBagConstraints();
+            tmp.gridwidth = 3;
+            tmp.gridheight = 3;
+            tmp.gridx = 0;
+            tmp.gridy = allUnitButtons.indexOf(i) * 3 + 1;
+            tmp.weightx = 1;
+            tmp.weighty = 0.5;
+
+            unitPane.add(i, tmp);
+
+        }
     }
 
     /*
      * configures and returns a button used for Unit Recruitment
      */
     private JButton getUnitRecruitmentButton(String name, Class unit) {
-
+        JButton x = new JButton();
+        x.setName(name);
+        return x;
     }
 
 }
