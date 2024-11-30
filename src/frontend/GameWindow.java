0a125618c1cce5efb5cc7cf40f4d6a205da6f7ae
Moritz Hoerkens <mhoerkens@gmail.com>
2024-11-30 12:28:43 +0100

    not much

--- src/frontend/GameWindow.java
+++ src/frontend/GameWindow.java
@@ -19,6 +19,7 @@ import backend.units.Unit;
 public class GameWindow extends JFrame {
     private JSplitPane unitFieldSplit;// holds the COntainer for the Units and the Playingfield
     private JPanel unitPane;
+    private JPanel fieldPane;
 
     public static void main(String[] args) {
         GameWindow x = new GameWindow();
@@ -46,6 +47,13 @@ public class GameWindow extends JFrame {
         unitFieldSplit.setDividerLocation(0.2);
     }
 
+    private void initailizeField(){
+        fieldPane= new JPanel();
+        unitFieldSplit.setRightComponent(fieldPane);
+        GridLayout layout
+        fieldPane.setLayout(getLayout());
+    }
+
     private void initailizeUnitCards() {
         unitPane = new JPanel();
         unitFieldSplit.setLeftComponent(unitPane);
@@ -54,9 +62,14 @@ public class GameWindow extends JFrame {
 
         // initalize all buttons for unit recruitment
         ArrayList<JButton> allUnitButtons = new ArrayList<JButton>();
-        allUnitButtons.add(getUnitRecruitmentButton("Test1", null, "src\\images\\black.png"));
+        allUnitButtons.add(getUnitRecruitmentButton("Test1", null, "/images/black.png"));
         allUnitButtons.add(getUnitRecruitmentButton("Test2", null, null));
         allUnitButtons.add(getUnitRecruitmentButton("Test3", null, null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test4", null, null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test4", null, null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test4", null, null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test4", null, null));
+        allUnitButtons.add(getUnitRecruitmentButton("Test4", null, null));
         allUnitButtons.add(getUnitRecruitmentButton("Test4", null, null));
         for (int i = 0; i < allUnitButtons.size(); i++) {
             JButton current = allUnitButtons.get(i);
@@ -78,6 +91,7 @@ public class GameWindow extends JFrame {
     /*
      * configures and returns a button used for Unit Recruitment
      */
+    // TODO ICONS NOT WORKING
     @SuppressWarnings("rawtypes")
     private JButton getUnitRecruitmentButton(String name, Class unit, String iconPath) {
         JButton x = new JButton();
@@ -85,9 +99,8 @@ public class GameWindow extends JFrame {
         // set the icon
         if (iconPath != null) {
             try {
-                Icon icon = new ImageIcon(ImageIcon.class.getResource(iconPath));
+                Icon icon = new ImageIcon(iconPath);
                 x.setIcon(icon);
-                System.out.print(iconPath + ":" + icon.getIconHeight());
             } catch (Exception e) {
                 throw e;
             }
---
+++ src/frontend/Map.java
@@ -0,0 +1,12 @@
+package frontend;
+
+import javax.swing.JPanel;
+
+public class Map extends JPanel {
+    private int[] dimensions;
+
+    private JButton
+    public Map(){
+        // create buttons
+    }
+}
